05b1d1a6852508dc061cf6fd882f167e32a15915
E.Azer Koçulu <azer@kodfabrik.com>
2014-09-07 21:45:08 -0700

    skip if ext is empty

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var mime = require("mime");
+var ext = require("path").extname;
 
 module.exports = setContentType;
 
 function setContentType (req, res) {
+  if (!ext(req.url)) return;
   res.setHeader('Content-Type', mime.lookup(req.url));
 }
